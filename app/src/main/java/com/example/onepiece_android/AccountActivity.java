e29cbdc2a8206dc06c9770c4da6b6b2b0e2b5585
Hong Seoeun <hseun0732@dsm.hs.kr>
2023-07-09 22:43:24 +0900

    📑 :: account_activity

---
+++ app/src/main/java/com/example/onepiece_android/AccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.onepiece_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
