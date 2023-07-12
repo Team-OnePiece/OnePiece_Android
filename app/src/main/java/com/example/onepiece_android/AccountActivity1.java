751d47df4d3f8046308513cd277edb639fd349b2
Hong Seoeun <hseun0732@dsm.hs.kr>
2023-07-12 18:47:15 +0900

    📑:: add account_activity1

--- app/src/main/java/com/example/onepiece_android/AccountActivity1.java
+++ app/src/main/java/com/example/onepiece_android/AccountActivity1.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AccountActivity extends AppCompatActivity {
+public class AccountActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        setContentView(R.layout.activity_account1);
     }
 }
