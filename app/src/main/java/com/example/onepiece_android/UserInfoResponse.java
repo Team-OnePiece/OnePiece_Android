e1468c0f34459cafa6d8e5995a63340d575a34e6
Hong Seoeun <hseun0732@dsm.hs.kr>
2023-08-11 05:05:50 +0900

    ⚡️:: MyPage 기능 구현

---
+++ app/src/main/java/com/example/onepiece_android/UserInfoResponse.java
@@ -0,0 +1,14 @@
+package com.example.onepiece_android;
+
+public class UserInfoResponse {
+    private String nickname;
+    private String profileImageUrl;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+}
