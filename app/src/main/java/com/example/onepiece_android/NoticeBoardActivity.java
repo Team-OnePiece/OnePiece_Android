2b3438fd117b30acd2c774f5d3beaa2aea19bf38
Hong Seoeun <hseun0732@dsm.hs.kr>
2023-08-16 03:27:06 +0900

    📝:: baseUrl
    baseUrl 수정 및 사용하지 않는 import 구문 삭제

--- app/src/main/java/com/example/onepiece_android/NoticeBoardActivity.java
+++ app/src/main/java/com/example/onepiece_android/NoticeBoardActivity.java
@@ -5,8 +5,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.onepiece_android.databinding.ActivityNoticeBoardBinding;
