55bc15a3b83479d57b0ef0479437625026e63f6d
양윤호 <yangyoonho0710@naver.com>
2023-08-19 22:27:35 +0900

    ⚡️ :: 게시물 등록 뷰 수정

--- app/src/main/java/com/example/onepiece_android/PostUpActivity.java
+++ app/src/main/java/com/example/onepiece_android/PostUpActivity.java
@@ -13,6 +13,7 @@ import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,12 +21,6 @@ import com.example.onepiece_android.databinding.ActivityPostUpBinding;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 
-<<<<<<< Updated upstream
-public class PostUpActivity extends AppCompatActivity {
-
-
-    private ActivityPostUpBinding binding;
-=======
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,31 +31,30 @@ import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
->>>>>>> Stashed changes
 
 public class PostUpActivity extends AppCompatActivity {
+
     private ActivityPostUpBinding binding;
     private ActivityResultLauncher<Intent> galleryLauncher;
     private List<String> enteredTags = new ArrayList<>();
     private TagApi tagApi;
-
+    private ServerApi serverApi;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityPostUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-<<<<<<< Updated upstream
-=======
         tagApi = new TagApi(this);
+        serverApi = PostUpRequest.getClient().create(ServerApi.class);
+
         ChipGroup chipGroup = binding.chipGroup;
 
         TextView tvPlus = binding.tvPlus;
         tvPlus.setOnClickListener(v -> {
             String tagText = binding.etTag.getText().toString().trim();
-            if(!tagText.isEmpty()){
-                if(enteredTags.size() < 6 && tagText.length() <= 5){
+            if (!tagText.isEmpty()) {
+                if (enteredTags.size() < 6 && tagText.length() <= 5) {
                     Chip chip = new Chip(PostUpActivity.this);
                     chip.setText(tagText);
                     chip.setCloseIconVisible(true);
@@ -81,7 +75,7 @@ public class PostUpActivity extends AppCompatActivity {
                     binding.etTag.setText("");
                 } else {
                     String toastMessage;
-                    if(enteredTags.size() >= 6){
+                    if (enteredTags.size() >= 6) {
                         toastMessage = "태그는 최대 6개까지 추가할 수 있습니다.";
                     } else {
                         toastMessage = "태그는 최대 5글자까지 작성할 수 있습니다.";
@@ -91,39 +85,34 @@ public class PostUpActivity extends AppCompatActivity {
             }
         });
 
-        ServerApi serverApi = PostUpRequest.getClient().create(ServerApi.class);
-
-
         binding.tvOkay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String place = binding.etPlace.getText().toString();
 
-
-                if(!place.isEmpty()){
+                if (!place.isEmpty()) {
                     RequestBody placeRequestBody = RequestBody.create(MediaType.parse("text/plain"), place);
                     MultipartBody.Part imagePart = MultipartBody.Part.createFormData("place", place, placeRequestBody);
 
                     String accessToken = "Bearer <access-token>";
                     int groupId = 1;
 
-
                     Call<PostUpResponse> call = serverApi.postUp(groupId, imagePart, place, accessToken);
 
                     call.enqueue(new Callback<PostUpResponse>() {
                         @Override
                         public void onResponse(Call<PostUpResponse> call, Response<PostUpResponse> response) {
-                            if(response.isSuccessful()) {
+                            if (response.isSuccessful()) {
                                 int feedId = response.body().getFeed_id();
                                 Toast.makeText(PostUpActivity.this, "서버 응답 성공 Feed ID : " + feedId, Toast.LENGTH_SHORT).show();
-                            } else{
-                                Toast.makeText(PostUpActivity.this,"통신 실패 : " + response.message(), Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(PostUpActivity.this, "통신 실패 : " + response.message(), Toast.LENGTH_SHORT).show();
                             }
                         }
 
                         @Override
                         public void onFailure(Call<PostUpResponse> call, Throwable t) {
-                            Toast.makeText(PostUpActivity.this, "통신 실패 : "+ t.getMessage(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(PostUpActivity.this, "통신 실패 : " + t.getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     });
                 } else {
@@ -132,27 +121,11 @@ public class PostUpActivity extends AppCompatActivity {
             }
         });
 
-
-
-        TextView textView = binding.tvSpinner;
-        binding.spGroup.setAdapter(ArrayAdapter.createFromResource(this, R.array.arr_group, android.R.layout.simple_spinner_item));
-        binding.spGroup.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                textView.setText(getResources().getStringArray(R.array.arr_group)[position]);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
->>>>>>> Stashed changes
-
         binding.tvPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String tagText = binding.etTag.getText().toString().trim();
-                if(!tagText.isEmpty()){
+                if (!tagText.isEmpty()) {
                     enteredTags.add(tagText);
                     Toast.makeText(PostUpActivity.this, "태그가 추가되었습니다." + tagText, Toast.LENGTH_SHORT).show();
 
@@ -184,7 +157,7 @@ public class PostUpActivity extends AppCompatActivity {
         galleryLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
-                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null){
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
                         Uri selectedImageUri = result.getData().getData();
                         try {
                             Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
@@ -204,33 +177,32 @@ public class PostUpActivity extends AppCompatActivity {
         galleryLauncher.launch(intent);
     }
 
-    private void uploadImage(File imageFile, int groupId){
+    private void uploadImage(File imageFile, int groupId) {
         RequestBody requestBody = RequestBody.create(MediaType.parse("image/*"), imageFile);
-        MultipartBody.Part imagePart = MultipartBody.Part.createFormData("image", imageFile.getName(),requestBody);
+        MultipartBody.Part imagePart = MultipartBody.Part.createFormData("image", imageFile.getName(), requestBody);
 
-       RequestBody placeRequestBody = RequestBody.create(MediaType.parse("text/plain"),binding.etPlace.getText().toString());
+        RequestBody placeRequestBody = RequestBody.create(MediaType.parse("text/plain"), binding.etPlace.getText().toString());
 
-       String accessToken = "Bearer <access-token>";
-       ServerApi serverApi = PostUpRequest.getClient().create(ServerApi.class);
-       Call<PostUpResponse> call = serverApi.postUp(groupId, imagePart, placeRequestBody.toString(), accessToken);
+        String accessToken = "Bearer <access-token>";
 
-       call.enqueue(new Callback<PostUpResponse>() {
-           @Override
-           public void onResponse(Call<PostUpResponse> call, Response<PostUpResponse> response) {
-               if(response.isSuccessful()){
-                   PostUpResponse postUpResponse = response.body();
-                   int feedId = postUpResponse.getFeed_id();
-                   Toast.makeText(PostUpActivity.this, "이미지 업로드 성공. Feed ID : " + feedId, Toast.LENGTH_SHORT).show();
-               } else {
-                   Toast.makeText(PostUpActivity.this, "이미지 업로드 실패", Toast.LENGTH_SHORT).show();
-               }
-           }
+        Call<PostUpResponse> call = serverApi.postUp(groupId, imagePart, placeRequestBody.toString(), accessToken);
 
-           @Override
-           public void onFailure(Call<PostUpResponse> call, Throwable t) {
-                Toast.makeText(PostUpActivity.this,"통신 실패 : " + t.getMessage(), Toast.LENGTH_SHORT).show();
-           }
-       });
+        call.enqueue(new Callback<PostUpResponse>() {
+            @Override
+            public void onResponse(Call<PostUpResponse> call, Response<PostUpResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    PostUpResponse postUpResponse = response.body();
+                    int feedId = postUpResponse.getFeed_id();
+                    Toast.makeText(PostUpActivity.this, "이미지 업로드 성공. Feed ID : " + feedId, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(PostUpActivity.this, "이미지 업로드 실패", Toast.LENGTH_SHORT).show();
+                }
+            }
 
+            @Override
+            public void onFailure(Call<PostUpResponse> call, Throwable t) {
+                Toast.makeText(PostUpActivity.this, "통신 실패 : " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
